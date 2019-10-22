4ad6b69ee4e54133601e5af4245f0b4a1833f037
jamaltan31 <jamaltan31@gmail.com>
2019-10-22 15:40:24 +0800

    fix styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import "../App.css";
+import { Button, Form, FormControl } from "react-bootstrap";
 
-const NavBar = () => {
+// const NavBar = () => {
+//   return (
+//     <nav className="navbar">
+//       <Link to="/"> Home </Link>
+//       <Link to="/users/2"> My Profile </Link>
+//       <Link to="/login"> Login </Link>
+//     </nav>
+//   );
+// };
+
+const TopNavBar = () => {
   return (
-    <nav className="navbar">
-      <Link to="/"> Home </Link>
-      <Link to="/users/2"> My Profile </Link>
-    </nav>
+    <div className="top-navbar">
+      <Navbar>
+        <NavbarBrand href="/">Nextagram</NavbarBrand>
+
+        <Nav>
+          <NavItem>
+            <NavLink href="/users/2">My Profile</NavLink>
+          </NavItem>
+
+          <NavItem>
+            <NavLink href="/login">Login</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
   );
 };
-export default NavBar;
+export default TopNavBar;
+
+// const TopNavBar = () => {
+//   return (
+//     <div>
+//       <Navbar bg="dark" variant="dark">
+//         <Navbar.Brand href="/">Nextagram</Navbar.Brand>
+//         <Nav className="mr-auto">
+//           <Nav.Link href="/users/2">My Profile</Nav.Link>
+//           <Nav.Link href="/login">Login</Nav.Link>
+//         </Nav>
+//         <Form inline>
+//           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+//           <Button variant="outline-info">Search</Button>
+//         </Form>
+//       </Navbar>
+//     </div>
+//   );
+// };
+
+// export default TopNavBar;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Card, Row, Col } from "reactstrap";
 import UserImages from "../components/UserImages";
+import "../App.css";
 
 class HomePage extends React.Component {
   render() {
@@ -13,12 +14,12 @@ class HomePage extends React.Component {
             <Card>
               <Row noGutters form>
                 <Col xs="2">
-                  <h4>{user.username}</h4>
                   <img
                     className="w-50 rounded-circle mx-auto d-block"
                     src={user.profileImage}
                     alt={user.username}
                   />
+                  <p className="each-user-name">{user.username} </p>
                 </Col>
                 <Col style={{ height: "300px" }}>
                   <UserImages userId={user.id} />
