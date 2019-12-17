141ef5d1c004592162ea96ec9fe9c78c685844d5
Dwi <jamaltan31@gmail.com>
2019-12-18 00:19:38 +0800

    super basic landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,7 +5,15 @@ class LandingPage extends React.Component {
   render() {
     return (
       <>
-        <div className="App-header">Landing Page here</div>
+        <div className="display-1 mb-5">Welcome to Nextagram!</div>
+        <div>So far you can do : </div>
+        <ul>
+          <li>Create new user / login existing user</li>
+          <li>
+            See all users that ever existed, and see each of their profile
+          </li>
+          <li>Upload images to your feed</li>
+        </ul>
       </>
     );
   }
